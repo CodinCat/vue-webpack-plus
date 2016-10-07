f00e599a9951b6c397f5f58f6b9398e705f45068
CodinCat <a55951234@gmail.com>
2016-10-07 19:03:31 +0800

    Add mutations unit test

---
+++ template/test/unit/specs/mutations.spec.js
@@ -0,0 +1,9 @@
+import * as mutations from 'src/store/mutations'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+
+describe('mutations', () => {
+  it('INCREMENT', () => {
+    const state = { count: 0 }{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+    mutations.INCREMENT(state){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+    expect(state.count).to.equal(1){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+  }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+}){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
