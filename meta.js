6a906ef7e50979dd45645cbddb3c3429bce99bab
CodinCat <a55951234@gmail.com>
2017-05-05 15:21:27 +0800

    [Upstream] Fixed naming of Airbnb

--- meta.js
+++ meta.js
@@ -55,9 +55,9 @@ module.exports = {
           "short": "Standard"
         },
         {
-          "name": "AirBNB (https://github.com/airbnb/javascript)",
+          "name": "Airbnb (https://github.com/airbnb/javascript)",
           "value": "airbnb",
-          "short": "AirBNB"
+          "short": "Airbnb"
         },
         {
           "name": "none (configure it yourself)",
